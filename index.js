08f6549e67fd4acc07fbd45064f10c319b420b98
FahadPatel72 <patelfahad92@gmail.com>
2024-10-13 16:19:29 +0530

    initialC

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ app.use(cors());
 app.use(fileUpload());
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000' 
+}));
 
 const dbConnect = require("./config/database");
 dbConnect();
