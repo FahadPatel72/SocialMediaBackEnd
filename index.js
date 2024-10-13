5abd0ba939437db170f23d40ff7d5bdb547f3273
FahadPatel72 <patelfahad92@gmail.com>
2024-10-13 16:10:26 +0530

    latestC

--- index.js
+++ index.js
@@ -13,9 +13,7 @@ app.use(cors());
 app.use(fileUpload());
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-app.use(cors({
-  origin: 'https://socialmediaf.netlify.app/' 
-}));
+app.use(cors());
 
 const dbConnect = require("./config/database");
 dbConnect();
